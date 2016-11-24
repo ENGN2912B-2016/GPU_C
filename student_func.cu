3679d64f83a70294627d0d76da995ee53648d765
Alvin <jiacheng_guo@brown.edu>
2016-11-24 01:52:38 -0500

    Optimized the mapping method

--- student_func.cu
+++ student_func.cu
@@ -10,17 +10,10 @@ void rgba_to_greyscale(const uchar4* const rgbaImage,
 	//First create a mapping from the 2D block and grid locations
 	//to an absolute 2D location in the image, then use that to
 	//calculate a 1D offset
-	//  int n = threadIdx.x;
-	//  int m = blockIdx.x;
-	//  uchar4 rgba=rgbaImage[numCols*m+n];
-	//  greyImage[numCols*m+n]=.299f * rgba.x + .587f * rgba.y + .114f * rgba.z;
-	int x = blockIdx.x * blockDim.x + threadIdx.x;
-	int y = blockIdx.y * blockDim.y + threadIdx.y;
-	if(x <= numCols && y <= numRows)
-	{
-		uchar4 rgba=rgbaImage[numCols*y+x];
-		greyImage[numCols*y+x]=.299f * rgba.x + .587f * rgba.y + .114f * rgba.z;
-	}
+	  int n = threadIdx.x;
+	  int m = blockIdx.x;
+	  uchar4 rgba=rgbaImage[numCols*m+n];
+	  greyImage[numCols*m+n]=.299f * rgba.x + .587f * rgba.y + .114f * rgba.z;
 }
 
 void your_rgba_to_greyscale(const uchar4 * const h_rgbaImage, uchar4 * const d_rgbaImage,
@@ -28,8 +21,8 @@ void your_rgba_to_greyscale(const uchar4 * const h_rgbaImage, uchar4 * const d_r
 {
   //You must fill in the correct sizes for the blockSize and gridSize
   //currently only one block with one thread is being launched
-  const dim3 blockSize(32, 32, 1);  //TODO
-  const dim3 gridSize(numCols/32+1, numRows/32+1, 1);  //TODO
+  const dim3 blockSize(numCols, 1, 1);  //TODO
+  const dim3 gridSize(numRows, 1, 1);  //TODO
   rgba_to_greyscale<<<gridSize, blockSize>>>(d_rgbaImage, d_greyImage, numRows, numCols);
   
   cudaDeviceSynchronize(); 
